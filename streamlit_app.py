f3f760fb8bb164bfb63789a58bbd32fe2fba3f1b
haderbill <109163038+haderbill@users.noreply.github.com>
2022-07-13 10:46:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title("My Parents are Trying to Put the Neighbor in the Hamburger Meat")
+streamlit.title("My Parents are Trying to Put the Neighbors in the Hamburger Meat")
+streamlit.header("Burger Menu")
+streamlit.text("Bob")
+streamlit.text("Janette")
+streamlit.text("Michael")
+streamlit.text("Anna")
