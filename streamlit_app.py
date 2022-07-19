39ed15de7c466239181d15b732ab86fae0ec1c82
haderbill <109163038+haderbill@users.noreply.github.com>
2022-07-19 10:16:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 streamlit.title("My Parents are Trying to Put the Neighbors in the Hamburger Meat")
 streamlit.header("Burger Menu")
